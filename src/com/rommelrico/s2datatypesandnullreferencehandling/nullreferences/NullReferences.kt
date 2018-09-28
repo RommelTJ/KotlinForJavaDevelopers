6411bcc49a113bac4589b01bc8f0f50f98136640
Rommel Rico <me@rommelrico.com>
2018-09-28 08:31:02 -0700

    Assigning a default with the Elvis operator

--- src/com/rommelrico/s2datatypesandnullreferencehandling/nullreferences/NullReferences.kt
+++ src/com/rommelrico/s2datatypesandnullreferencehandling/nullreferences/NullReferences.kt
@@ -2,7 +2,7 @@ package com.rommelrico.s2datatypesandnullreferencehandling.nullreferences
 
 fun main(args: Array<String>) {
     // val str: String = null // Can't do this. Null cannot be a value of non-null type String
-    val str: String? = "This isn't null" // This is ok.
+    var str: String? = "This isn't null" // This is ok.
     // str.toUpperCase() // can't do this because non-safe calls are not allowed.
     // Safe-call operator = '?', is short-hand for 'if (str != null) { }'
     println(str?.toUpperCase())
@@ -11,4 +11,10 @@ fun main(args: Array<String>) {
 
     // If one of the items in the chain is null, it short-circuits to null. See verbose example in NullReferences.java.
     // val countryCode: String? = bankBranch?.address?.country?.countryCode
+
+    // Elvis Operator '?:' is used to assign a default instead of a null.
+    str = null
+    val str2 = str?: "This is default value"
+    println(str)
+    println(str2)
 }
