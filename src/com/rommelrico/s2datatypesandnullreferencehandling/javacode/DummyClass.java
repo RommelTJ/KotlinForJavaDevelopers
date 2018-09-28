de9b10774e4b790aba8def40b4587cd2d838d218
Rommel Rico <me@rommelrico.com>
2018-09-27 20:51:26 -0700

    Dummy Java Class

---
+++ src/com/rommelrico/s2datatypesandnullreferencehandling/javacode/DummyClass.java
@@ -0,0 +1,9 @@
+package com.rommelrico.s2datatypesandnullreferencehandling.javacode;
+
+public class DummyClass {
+
+    public String isVacationTime(boolean onVacation) {
+        return onVacation ? "I'm on vacation!": "I'm working";
+    }
+
+}
