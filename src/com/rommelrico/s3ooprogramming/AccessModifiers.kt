e9d8b5b805e1fb449afad9625f7ea8ef6f977e68
Rommel Rico <me@rommelrico.com>
2018-09-29 16:30:32 -0700

    Example of using a private class in Kotlin

--- src/com/rommelrico/s3ooprogramming/AccessModifiers.kt
+++ src/com/rommelrico/s3ooprogramming/AccessModifiers.kt
@@ -4,5 +4,9 @@ fun main(args: Array<String>) {
     // Top level items are public by default in Kotlin (vs package-private)
     // In Kotlin, private top-level items mean everything in same file can access
     // In Kotlin, no matching requirement between Class names and filenames.
-    
+
+    val m = Employee()
+    println(m)
 }
+
+private class Employee
