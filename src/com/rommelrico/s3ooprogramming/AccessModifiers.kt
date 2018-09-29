26797a0671ea3b00e43792422ea3fdf8ee62435d
Rommel Rico <me@rommelrico.com>
2018-09-29 16:28:26 -0700

    Top-level access modifiers notes

--- src/com/rommelrico/s3ooprogramming/AccessModifiers.kt
+++ src/com/rommelrico/s3ooprogramming/AccessModifiers.kt
@@ -1,5 +1,8 @@
 package com.rommelrico.s3ooprogramming
 
 fun main(args: Array<String>) {
+    // Top level items are public by default in Kotlin (vs package-private)
+    // In Kotlin, private top-level items mean everything in same file can access
+    // In Kotlin, no matching requirement between Class names and filenames.
     
 }
