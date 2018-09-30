b14a3003160e964426d2b5387d4084423b9dfa13
Rommel Rico <me@rommelrico.com>
2018-09-30 09:37:37 -0700

    Sample Java Class

---
+++ src/com/rommelrico/s3ooprogramming/javacode/JavaEmployee.java
@@ -0,0 +1,11 @@
+package com.rommelrico.s3ooprogramming.javacode;
+
+public class JavaEmployee {
+
+    private final String firstName;
+
+    public JavaEmployee(String firstName) {
+        this.firstName = firstName;
+    }
+    
+}
