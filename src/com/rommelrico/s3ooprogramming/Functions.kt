b148309388550b402d607c5c9fdecdc3f028c537
Rommel Rico <me@rommelrico.com>
2018-10-02 08:38:05 -0700

    Functions in Classes and how to call them

--- src/com/rommelrico/s3ooprogramming/Functions.kt
+++ src/com/rommelrico/s3ooprogramming/Functions.kt
@@ -2,6 +2,8 @@ package com.rommelrico.s3ooprogramming
 
 fun main(args: Array<String>): Unit {
     println(labelMultiply(5, 4, "The result is: "))
+    val e1 = Employee2("John")
+    println(e1.upperCaseFirstName())
 }
 
 /*
@@ -13,3 +15,6 @@ fun labelMultiply(operand1: Int, operand2: Int, label: String): String {
 // Shorter way
 fun labelMultiply(operand1: Int, operand2: Int, label: String): String = "$label ${operand1 * operand2}"
 
+class Employee2(val firstName: String) {
+    fun upperCaseFirstName() = firstName.toUpperCase()
+}
