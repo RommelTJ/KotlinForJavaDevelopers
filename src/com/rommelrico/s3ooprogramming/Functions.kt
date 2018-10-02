26c4b229b6242a81316e98fd986640f6b73317f4
Rommel Rico <me@rommelrico.com>
2018-10-02 08:34:23 -0700

    Making the function shorter

--- src/com/rommelrico/s3ooprogramming/Functions.kt
+++ src/com/rommelrico/s3ooprogramming/Functions.kt
@@ -1,10 +1,15 @@
 package com.rommelrico.s3ooprogramming
 
 fun main(args: Array<String>): Unit {
-    println(labelMultiply(3, 4, "The result is: "))
+    println(labelMultiply(5, 4, "The result is: "))
 }
 
+/*
 fun labelMultiply(operand1: Int, operand2: Int, label: String): String {
     return ("$label ${operand1 * operand2}")
 }
+*/
+
+// Shorter way
+fun labelMultiply(operand1: Int, operand2: Int, label: String): String = "$label ${operand1 * operand2}"
 
