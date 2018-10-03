e19d43f32c31c09e2e8f3592b85f274f304ddfb9
Rommel Rico <me@rommelrico.com>
2018-10-03 08:28:08 -0700

    Overriding the printModel function

--- src/com/rommelrico/s3ooprogramming/inheritance/Printer.kt
+++ src/com/rommelrico/s3ooprogramming/inheritance/Printer.kt
@@ -4,10 +4,13 @@ package com.rommelrico.s3ooprogramming.inheritance
 // Since we declared the class as 'abstract', we no longer need 'open' since it's redundant because abstract classes
 // have to be extended.
 abstract class Printer(val modelName: String) {
-    fun printModel() = println("The model name of this printer is: $modelName")
+    open fun printModel() = println("The model name of this printer is: $modelName")
 }
 
-class LaserPrinter(modelName: String): Printer(modelName)
+class LaserPrinter(modelName: String): Printer(modelName) {
+    // Requires 'override' keyword and base function to be 'open'.
+    override fun printModel() = println("The model name of this laser printer is: $modelName")
+}
 
 fun main(args: Array<String>) {
     val laserPrinter = LaserPrinter("Brother")
