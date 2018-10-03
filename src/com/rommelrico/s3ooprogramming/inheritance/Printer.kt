edc8ddd8a2f4881c65be8478a2e69a3998fa4a2e
Rommel Rico <me@rommelrico.com>
2018-10-03 08:24:54 -0700

    Notes on abstract classes

--- src/com/rommelrico/s3ooprogramming/inheritance/Printer.kt
+++ src/com/rommelrico/s3ooprogramming/inheritance/Printer.kt
@@ -1,6 +1,8 @@
 package com.rommelrico.s3ooprogramming.inheritance
 
 // To tell compiler that this class can be extended we add 'open'
+// Since we declared the class as 'abstract', we no longer need 'open' since it's redundant because abstract classes
+// have to be extended.
 abstract class Printer(val modelName: String)
 
 class LaserPrinter(modelName: String): Printer(modelName)
