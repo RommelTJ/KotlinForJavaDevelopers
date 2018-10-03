b6a56ce341bbf374332a0c99584f8d10a861e3ad
Rommel Rico <me@rommelrico.com>
2018-10-03 08:23:51 -0700

    Removing redundant open keyword because abstract has to be extended

--- src/com/rommelrico/s3ooprogramming/inheritance/Printer.kt
+++ src/com/rommelrico/s3ooprogramming/inheritance/Printer.kt
@@ -1,14 +1,6 @@
 package com.rommelrico.s3ooprogramming.inheritance
 
 // To tell compiler that this class can be extended we add 'open'
-open abstract class Printer(val modelName: String) {
+abstract class Printer(val modelName: String)
 
-
-
-}
-
-class LaserPrinter(modelName: String): Printer(modelName) {
-
-
-
-}
+class LaserPrinter(modelName: String): Printer(modelName)
