8904231ec5b7f361074408d1275de0507ccf5acb
Rommel Rico <me@rommelrico.com>
2018-10-02 19:02:20 -0700

    upperFirstAndLast function

--- src/com/rommelrico/s3ooprogramming/Utils.kt
+++ src/com/rommelrico/s3ooprogramming/Utils.kt
@@ -2,5 +2,11 @@ package com.rommelrico.s3ooprogramming
 
 class Utils {
 
+    fun upperFirstAndLast(str: String): String {
+        val upperFirst = str.substring(0, 1).toUpperCase() + str.substring(1)
+        return upperFirst.substring(0, upperFirst.length - 1)+
+                upperFirst.substring(upperFirst.length - 1, upperFirst.length).toUpperCase()
+    }
+
 }
 
