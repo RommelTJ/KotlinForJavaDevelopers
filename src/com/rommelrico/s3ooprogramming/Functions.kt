cb287245636d407eb97e070e1140591698c1d848
Rommel Rico <me@rommelrico.com>
2018-10-02 19:08:38 -0700

    this is not required since String instance is inferred

--- src/com/rommelrico/s3ooprogramming/Functions.kt
+++ src/com/rommelrico/s3ooprogramming/Functions.kt
@@ -1,7 +1,7 @@
 package com.rommelrico.s3ooprogramming
 
 fun String.upperFirstAndLast(): String {
-    val upperFirst = this.substring(0, 1).toUpperCase() + this.substring(1)
+    val upperFirst = substring(0, 1).toUpperCase() + substring(1)
     return upperFirst.substring(0, upperFirst.length - 1)+
             upperFirst.substring(upperFirst.length - 1, upperFirst.length).toUpperCase()
 }
