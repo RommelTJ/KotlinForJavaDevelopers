4ec990a327eee9e1a2da2d5873b7319d95ef447b
Rommel Rico <me@rommelrico.com>
2018-10-03 08:26:34 -0700

    Adding a function and using it on an instance of a subclass

--- src/com/rommelrico/s3ooprogramming/inheritance/Printer.kt
+++ src/com/rommelrico/s3ooprogramming/inheritance/Printer.kt
@@ -3,6 +3,13 @@ package com.rommelrico.s3ooprogramming.inheritance
 // To tell compiler that this class can be extended we add 'open'
 // Since we declared the class as 'abstract', we no longer need 'open' since it's redundant because abstract classes
 // have to be extended.
-abstract class Printer(val modelName: String)
+abstract class Printer(val modelName: String) {
+    fun printModel() = println("The model name of this printer is: $modelName")
+}
 
 class LaserPrinter(modelName: String): Printer(modelName)
+
+fun main(args: Array<String>) {
+    val laserPrinter = LaserPrinter("Brother")
+    laserPrinter.printModel()
+}
