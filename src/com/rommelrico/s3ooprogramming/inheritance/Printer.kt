38de91839ceea64233ece4bfa4dbd482d5cf2128
Rommel Rico <me@rommelrico.com>
2018-10-03 08:21:39 -0700

    Adding a Primary Constructor

--- src/com/rommelrico/s3ooprogramming/inheritance/Printer.kt
+++ src/com/rommelrico/s3ooprogramming/inheritance/Printer.kt
@@ -1,11 +1,14 @@
 package com.rommelrico.s3ooprogramming.inheritance
 
 // To tell compiler that this class can be extended we add 'open'
-open class Printer {
+open class Printer() {
+
+
+
 }
 
-class LaserPrinter: Printer {
+class LaserPrinter(): Printer() {
 
-    constructor(): super()
+
 
 }
