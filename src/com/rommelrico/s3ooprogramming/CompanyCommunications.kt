384e7ae04abdf4eeaa5245223a99db251a0c6c89
Rommel Rico <me@rommelrico.com>
2018-10-03 19:49:41 -0700

    Kotlin Singleton

---
+++ src/com/rommelrico/s3ooprogramming/CompanyCommunications.kt
@@ -0,0 +1,20 @@
+package com.rommelrico.s3ooprogramming
+
+import java.time.Year
+
+// 'object' keyword means only one instance will be created.
+// Kotlin 'object' declaration declares and creates an instance at the same time.
+object CompanyCommunications {
+
+    val currentYear = Year.now().value
+
+    fun getTagLine() = "Our company rocks!"
+
+    fun getCopyrightLine() = "Copyright \u00A9 $currentYear Our Company. All rights reserved."
+
+}
+
+fun main(args: Array<String>) {
+    println(CompanyCommunications.getTagLine())
+    println(CompanyCommunications.getCopyrightLine())
+}
