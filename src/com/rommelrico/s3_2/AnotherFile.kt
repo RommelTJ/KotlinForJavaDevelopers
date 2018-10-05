960249354d96e25896b9fb7e0082bf993d29dc6e
Rommel Rico <me@rommelrico.com>
2018-10-04 20:06:41 -0700

    Importing an Enum

--- src/com/rommelrico/s3_2/AnotherFile.kt
+++ src/com/rommelrico/s3_2/AnotherFile.kt
@@ -2,8 +2,12 @@ package com.rommelrico.s3_2
 
 import com.rommelrico.s3ooprogramming.CompanyCommunications
 import com.rommelrico.s3ooprogramming.topLevel
+import com.rommelrico.s3ooprogramming.Department.IT
+import com.rommelrico.s3ooprogramming.Department.*
 
 fun main(args: Array<String>) {
     topLevel("Test")
     println(CompanyCommunications.getCopyrightLine())
+    println(IT.getDepartmentInfo())
+    println(HR.getDepartmentInfo())
 }
