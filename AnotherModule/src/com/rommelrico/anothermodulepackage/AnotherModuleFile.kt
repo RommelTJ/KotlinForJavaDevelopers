34204c891d9b00fec909cf5865a2b2aa94e4ef1d
Rommel Rico <me@rommelrico.com>
2018-10-04 20:10:04 -0700

    Importing an extension function into another module

--- AnotherModule/src/com/rommelrico/anothermodulepackage/AnotherModuleFile.kt
+++ AnotherModule/src/com/rommelrico/anothermodulepackage/AnotherModuleFile.kt
@@ -1,7 +1,9 @@
 package com.rommelrico.anothermodulepackage
 
 import com.rommelrico.s3ooprogramming.topLevel
+import com.rommelrico.s3ooprogramming.upperFirstAndLast
 
 fun main(args: Array<String>) {
     topLevel("Test2")
+    println("A string to work with".upperFirstAndLast())
 }
