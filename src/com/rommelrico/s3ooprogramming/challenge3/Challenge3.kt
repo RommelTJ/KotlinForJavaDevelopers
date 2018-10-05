07838e4c8f67a0c194ec21a5fef0fec787ab3caf
Rommel Rico <me@rommelrico.com>
2018-10-05 08:41:09 -0700

    Solution to Part 5

--- src/com/rommelrico/s3ooprogramming/challenge3/Challenge3.kt
+++ src/com/rommelrico/s3ooprogramming/challenge3/Challenge3.kt
@@ -10,13 +10,27 @@ open class KotlinBicycle(var cadence: Int, var gear: Int, var speed: Int) {
         speed += increment;
     }
 
-    fun printDescription() {
+    open fun printDescription() {
         println("Bike is in gear $gear with a cadence of $cadence traveling at a speed of $speed.")
     }
 
 }
 
-class KotlinMountainBike(var seatHeight: Int, cadence: Int, gear: Int, speed: Int): KotlinBicycle(cadence, gear, speed)
+class KotlinMountainBike(var seatHeight: Int, cadence: Int, gear: Int, speed: Int): KotlinBicycle(cadence, gear, speed) {
 
-class KotlinRoadBike(val tireWidth: Int, cadence: Int, gear: Int, speed: Int): KotlinBicycle(cadence, gear, speed)
+    override fun printDescription() {
+        super.printDescription()
+        println("The mountain bike has a seat height of $seatHeight inches.")
+    }
+
+}
+
+class KotlinRoadBike(val tireWidth: Int, cadence: Int, gear: Int, speed: Int): KotlinBicycle(cadence, gear, speed) {
+
+    override fun printDescription() {
+        super.printDescription()
+        println("The road bike has a tire width of " + tireWidth + "mm.")
+    }
+
+}
 
