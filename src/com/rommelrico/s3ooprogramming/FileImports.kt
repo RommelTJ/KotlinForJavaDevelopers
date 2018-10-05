14045ffb46c0b23b71ac72eb0aebeaf70df8e698
Rommel Rico <me@rommelrico.com>
2018-10-04 19:53:39 -0700

    Packages in Kotlin don't have to match directory structure

---
+++ src/com/rommelrico/s3ooprogramming/FileImports.kt
@@ -0,0 +1,6 @@
+package com.rommelrico.s3ooprogramming
+
+// Packages in Kotlin don't have to match the directory structure.
+fun main(args: Array<String>) {
+    println("Package doesn't match.")
+}
