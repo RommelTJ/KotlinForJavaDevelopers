ccdd2e059862da9b410308d1ee469efed68343cf
Rommel Rico <me@rommelrico.com>
2018-10-04 20:07:11 -0700

    Using aliases

--- src/com/rommelrico/s3_2/AnotherFile.kt
+++ src/com/rommelrico/s3_2/AnotherFile.kt
@@ -1,13 +1,13 @@
 package com.rommelrico.s3_2
 
-import com.rommelrico.s3ooprogramming.CompanyCommunications
+import com.rommelrico.s3ooprogramming.CompanyCommunications as Comm
 import com.rommelrico.s3ooprogramming.topLevel
 import com.rommelrico.s3ooprogramming.Department.IT
 import com.rommelrico.s3ooprogramming.Department.*
 
 fun main(args: Array<String>) {
     topLevel("Test")
-    println(CompanyCommunications.getCopyrightLine())
+    println(Comm.getCopyrightLine())
     println(IT.getDepartmentInfo())
     println(HR.getDepartmentInfo())
 }
