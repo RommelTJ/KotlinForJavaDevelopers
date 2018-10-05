d655bec0bb029d7e733d09c23dd5b7135f4cf50c
Rommel Rico <me@rommelrico.com>
2018-10-04 20:07:56 -0700

    Using aliases with functions

--- src/com/rommelrico/s3_2/AnotherFile.kt
+++ src/com/rommelrico/s3_2/AnotherFile.kt
@@ -1,12 +1,12 @@
 package com.rommelrico.s3_2
 
 import com.rommelrico.s3ooprogramming.CompanyCommunications as Comm
-import com.rommelrico.s3ooprogramming.topLevel
+import com.rommelrico.s3ooprogramming.topLevel as tp
 import com.rommelrico.s3ooprogramming.Department.IT
 import com.rommelrico.s3ooprogramming.Department.*
 
 fun main(args: Array<String>) {
-    topLevel("Test")
+    tp("Test")
     println(Comm.getCopyrightLine())
     println(IT.getDepartmentInfo())
     println(HR.getDepartmentInfo())
