6448abd8871e003e7193263090e8594caff907c9
Rommel Rico <me@rommelrico.com>
2018-10-04 20:04:41 -0700

    Importing a class

--- src/com/rommelrico/s3_2/AnotherFile.kt
+++ src/com/rommelrico/s3_2/AnotherFile.kt
@@ -1,7 +1,9 @@
 package com.rommelrico.s3_2
 
+import com.rommelrico.s3ooprogramming.CompanyCommunications
 import com.rommelrico.s3ooprogramming.topLevel
 
 fun main(args: Array<String>) {
     topLevel("Test")
+    println(CompanyCommunications.getCopyrightLine())
 }
