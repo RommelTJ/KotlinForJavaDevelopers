3ea9ba6270b16ca64a4bcd25e8355f6b1b9d1e8e
Rommel Rico <me@rommelrico.com>
2018-10-04 20:10:40 -0700

    Aliases with top level extension functions

--- AnotherModule/src/com/rommelrico/anothermodulepackage/AnotherModuleFile.kt
+++ AnotherModule/src/com/rommelrico/anothermodulepackage/AnotherModuleFile.kt
@@ -1,9 +1,9 @@
 package com.rommelrico.anothermodulepackage
 
 import com.rommelrico.s3ooprogramming.topLevel
-import com.rommelrico.s3ooprogramming.upperFirstAndLast
+import com.rommelrico.s3ooprogramming.upperFirstAndLast as ufal
 
 fun main(args: Array<String>) {
     topLevel("Test2")
-    println("A string to work with".upperFirstAndLast())
+    println("A string to work with".ufal())
 }
