d0d3a08a48fcdf7e465bf0c13cbcb277a3451b2a
Rommel Rico <me@rommelrico.com>
2018-10-05 08:29:47 -0700

    RoadBike Java Class

--- src/com/rommelrico/s3ooprogramming/javacode/RoadBike.java
+++ src/com/rommelrico/s3ooprogramming/javacode/RoadBike.java
@@ -1,4 +1,17 @@
 package com.rommelrico.s3ooprogramming.javacode;
 
-public class RoadBike {
+public class RoadBike extends Bicycle {
+
+    // In millimeters
+    private int tireWidth;
+
+    public RoadBike(int cadence, int gear, int speed, int tireWidth) {
+        super(cadence, gear, speed);
+        this.tireWidth = tireWidth;
+    }
+
+    public int getTireWidth() {
+        return tireWidth;
+    }
+    
 }
