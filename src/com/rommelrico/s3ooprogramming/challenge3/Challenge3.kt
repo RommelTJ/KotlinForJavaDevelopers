5c7a35c985f921d5e09976f221a92351066cdc8d
Rommel Rico <me@rommelrico.com>
2018-10-05 08:20:32 -0700

    Solution to part 1

--- src/com/rommelrico/s3ooprogramming/challenge3/Challenge3.kt
+++ src/com/rommelrico/s3ooprogramming/challenge3/Challenge3.kt
@@ -1,2 +1,14 @@
 package com.rommelrico.s3ooprogramming.challenge3
 
+class KotlinBicycle(var cadence: Int, var gear: Int, var speed: Int) {
+
+    fun applyBreak(decrement: Int) {
+        speed -= decrement
+    }
+
+    fun speedUp(increment: Int) {
+        speed += increment;
+    }
+
+}
+
