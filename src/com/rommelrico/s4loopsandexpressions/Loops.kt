585ddea465dcdce9c371200d3f3da62f3d80959c
Rommel Rico <me@rommelrico.com>
2018-10-06 16:55:21 -0700

    You can also have ranges for chars and Strings

--- src/com/rommelrico/s4loopsandexpressions/Loops.kt
+++ src/com/rommelrico/s4loopsandexpressions/Loops.kt
@@ -3,4 +3,9 @@ package com.rommelrico.s4loopsandexpressions
 fun main(args: Array<String>) {
     // Ranges in Kotlin are inclusive, so it includes 1 and 5.
     val range = 1..5
+
+    // You can also have ranges of chars and strings because they are comparable
+    val charRange = 'a'..'z'
+    val stringRange = "ABC".."XYZ"
+    
 }
