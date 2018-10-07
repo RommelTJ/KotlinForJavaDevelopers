7846b3dec57ebe809a9ee9d69494742f78e3c12a
Rommel Rico <me@rommelrico.com>
2018-10-06 17:24:14 -0700

    Assigning labels to loops

--- src/com/rommelrico/s4loopsandexpressions/Loops.kt
+++ src/com/rommelrico/s4loopsandexpressions/Loops.kt
@@ -83,4 +83,18 @@ fun main(args: Array<String>) {
     seasons.forEach { println(it) }
     seasons.forEachIndexed { index, value -> println("$value is season number $index") }
 
+    // Giving loops a name.
+    for (i in 1..3) {
+        println("i = $i")
+        jloop@ for (j in 1..4) {
+            println("j = $j")
+            for (k in 5..10) {
+                println("k = $k")
+                if (k == 7) {
+                    break@jloop
+                }
+            }
+        }
+    }
+
 }
