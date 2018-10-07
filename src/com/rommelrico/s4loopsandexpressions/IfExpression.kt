da8f2e6390cf11752e52201ce56ef4b889e30068
Rommel Rico <me@rommelrico.com>
2018-10-06 17:28:58 -0700

    Kotlin if expression

--- src/com/rommelrico/s4loopsandexpressions/IfExpression.kt
+++ src/com/rommelrico/s4loopsandexpressions/IfExpression.kt
@@ -3,4 +3,12 @@ package com.rommelrico.s4loopsandexpressions
 // if statement in Kotlin can be an expression that returns a value.
 fun main(args: Array<String>) {
 
+    val someCondition = 69 < 22
+//    var num: Int
+//    if (someCondition) {
+//        num = 50
+//    } else {
+//        num = 592
+//    }
+    val num = if (someCondition) 50 else 592
 }
