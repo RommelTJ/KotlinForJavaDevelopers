66416ac3f6c379db249976474b2260550c511780
Rommel Rico <me@rommelrico.com>
2018-10-07 19:32:23 -0700

    Performing same code for multiple values

--- src/com/rommelrico/s4loopsandexpressions/WhenExpression.kt
+++ src/com/rommelrico/s4loopsandexpressions/WhenExpression.kt
@@ -3,9 +3,9 @@ package com.rommelrico.s4loopsandexpressions
 fun main(args: Array<String>) {
 
     // Equivalent 'when' expression to SwitchExample.java
-    val num = 200
+    val num = 600
     when (num) {
-        100 -> println("100")
+        100, 600 -> println("600")
         200 -> println("200")
         300 -> println("300")
         else -> println("Doesn't match anything.")
