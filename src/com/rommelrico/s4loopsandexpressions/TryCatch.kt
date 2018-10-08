8d7b6e79e76d20290ea50452c4db1f8f92b7d8e3
Rommel Rico <me@rommelrico.com>
2018-10-07 20:00:42 -0700

    using the elvis operator in try catch block

--- src/com/rommelrico/s4loopsandexpressions/TryCatch.kt
+++ src/com/rommelrico/s4loopsandexpressions/TryCatch.kt
@@ -1,20 +1,21 @@
 package com.rommelrico.s4loopsandexpressions
 
+import java.lang.IllegalArgumentException
 import java.lang.NumberFormatException
 
 fun main(args: Array<String>) {
 
     // Using try and catch as an expression in Kotlin
     println("${getNumber("5")}")
-    println("${getNumber("abc")}")
+    println("${getNumber("abc") ?: throw IllegalArgumentException("Number isn't Int")}")
 
 }
 
-fun getNumber(str: String): Int {
+fun getNumber(str: String): Int? {
     return try {
         Integer.parseInt(str)
     } catch (e: NumberFormatException) {
-        0
+        null
     } finally {
         println("I'm in the finally block.")
         1 // not involved in value of try-catch block so won't be returned / will be ignored
