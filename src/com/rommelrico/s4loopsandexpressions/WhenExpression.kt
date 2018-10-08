aeb79be96d67368f7a53581987af7c9fe99d6cc0
Rommel Rico <me@rommelrico.com>
2018-10-07 19:30:27 -0700

    Kotlin when expression

--- src/com/rommelrico/s4loopsandexpressions/WhenExpression.kt
+++ src/com/rommelrico/s4loopsandexpressions/WhenExpression.kt
@@ -1,5 +1,14 @@
 package com.rommelrico.s4loopsandexpressions
 
 fun main(args: Array<String>) {
-    
+
+    // Equivalent 'when' expression to SwitchExample.java
+    val num = 200
+    when (num) {
+        100 -> println("100")
+        200 -> println("200")
+        300 -> println("300")
+        else -> println("Doesn't match anything.")
+    }
+
 }
