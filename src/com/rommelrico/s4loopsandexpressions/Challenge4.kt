2db40855bca2d09c0180521c70c8b4427f93dc10
Rommel Rico <me@rommelrico.com>
2018-10-07 20:23:40 -0700

    Challenge 4 Part 4

--- src/com/rommelrico/s4loopsandexpressions/Challenge4.kt
+++ src/com/rommelrico/s4loopsandexpressions/Challenge4.kt
@@ -14,17 +14,37 @@ fun main(args: Array<String>) {
 
     // Part 3
     // Print the first 15 numbers in the Fibonacci sequence using a range.
-    println(0)
-    println(1)
-    var total: Int
-    var secondToLast = 0
-    var last = 1
-    for (i in 1..13) {
-        total = secondToLast + last
-        println(total)
-        secondToLast = last
-        last = total
-    }
+//    println(0)
+//    println(1)
+//    var total: Int
+//    var secondToLast = 0
+//    var last = 1
+//    for (i in 1..13) {
+//        total = secondToLast + last
+//        println(total)
+//        secondToLast = last
+//        last = total
+//    }
 
+    // Part 4
+    firstLoop@ for (i in 1..5) {
+        if (i == 1) {
+            println(i)
+            continue
+        }
+        for (j in 11..20) {
+            println(j)
+            if (j == 11) continue
+            for (k in 100 downTo 90) {
+                println(k)
+                if (k == 98) {
+                    if (i == 2) {
+                        println(i)
+                    }
+                    break@firstLoop
+                }
+            }
+        }
+    }
 
 }
