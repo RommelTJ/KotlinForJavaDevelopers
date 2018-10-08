6ab7791b91d3c57f9b2591ecd48b0c785b00b10d
Rommel Rico <me@rommelrico.com>
2018-10-08 08:25:22 -0700

    Assing some Employees to a list

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Lambdas.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Lambdas.kt
@@ -2,6 +2,14 @@ package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
 
 fun main(args: Array<String>) {
     run { println("I'm in a lamdba!") }
+
+    val employees = listOf(
+            Employee("John", "Smith", 2012),
+            Employee("Jane", "Wilson", 2015),
+            Employee("Mary", "Johnson", 2010),
+            Employee("Mike", "Jones", 2002)
+    )
+    
 }
 
 class Employee(val firstName: String, val lastName: String, val startYear: Int)
