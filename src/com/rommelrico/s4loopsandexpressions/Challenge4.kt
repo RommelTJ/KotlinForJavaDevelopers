b64bc78ff64b3636579857b1c8f97a4e7cc5815c
Rommel Rico <me@rommelrico.com>
2018-10-07 20:12:54 -0700

    Challenge 4 Part 3

--- src/com/rommelrico/s4loopsandexpressions/Challenge4.kt
+++ src/com/rommelrico/s4loopsandexpressions/Challenge4.kt
@@ -3,13 +3,28 @@ package com.rommelrico.s4loopsandexpressions
 fun main(args: Array<String>) {
 
     // Part 1
-    for (num in 5..5000 step 5) {
-        println(num)
-    }
+//    for (num in 5..5000 step 5) {
+//        println(num)
+//    }
 
     // Part 2
-    for (num in -500..0) {
-        println(num)
+//    for (num in -500..0) {
+//        println(num)
+//    }
+
+    // Part 3
+    // Print the first 15 numbers in the Fibonacci sequence using a range.
+    println(0)
+    println(1)
+    var total: Int
+    var secondToLast = 0
+    var last = 1
+    for (i in 1..13) {
+        total = secondToLast + last
+        println(total)
+        secondToLast = last
+        last = total
     }
+
 
 }
