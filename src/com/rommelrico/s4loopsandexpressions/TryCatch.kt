a9a8f44e227052332e80f872e2a87da4802813f4
Rommel Rico <me@rommelrico.com>
2018-10-07 19:53:01 -0700

    Using try and catch as an expression in Kotlin

--- src/com/rommelrico/s4loopsandexpressions/TryCatch.kt
+++ src/com/rommelrico/s4loopsandexpressions/TryCatch.kt
@@ -1,5 +1,19 @@
 package com.rommelrico.s4loopsandexpressions
 
+import java.lang.NumberFormatException
+
 fun main(args: Array<String>) {
+
+    // Using try and catch as an expression in Kotlin
+    println("${getNumber("5")}")
+    println("${getNumber("abc")}")
     
+}
+
+fun getNumber(str: String): Int {
+    return try {
+        Integer.parseInt(str)
+    } catch (e: NumberFormatException) {
+        0
+    }
 }
