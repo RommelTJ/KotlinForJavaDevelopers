393c07a36864b4e381533197821edf55c8a8be6d
Rommel Rico <me@rommelrico.com>
2018-10-08 08:23:38 -0700

    Employee class

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Lambdas.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Lambdas.kt
@@ -3,3 +3,5 @@ package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
 fun main(args: Array<String>) {
     run { println("I'm in a lamdba!") }
 }
+
+class Employee(val firstName: String, val lastName: String, val startYear: Int)
