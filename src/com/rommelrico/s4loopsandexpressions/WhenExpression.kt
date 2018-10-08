3d088dceabd52b36249b6c3a36ebea34da413597
Rommel Rico <me@rommelrico.com>
2018-10-07 19:32:58 -0700

    Using ranges with when expressions

--- src/com/rommelrico/s4loopsandexpressions/WhenExpression.kt
+++ src/com/rommelrico/s4loopsandexpressions/WhenExpression.kt
@@ -3,9 +3,9 @@ package com.rommelrico.s4loopsandexpressions
 fun main(args: Array<String>) {
 
     // Equivalent 'when' expression to SwitchExample.java
-    val num = 600
+    val num = 150
     when (num) {
-        100, 600 -> println("600")
+        in 100..199 -> println("In range 100..199")
         200 -> println("200")
         300 -> println("300")
         else -> println("Doesn't match anything.")
