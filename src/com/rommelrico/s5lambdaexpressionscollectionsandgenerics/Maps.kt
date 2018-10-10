94878349c0d6387c87c8413284c0a4ec9818c1cc
Rommel Rico <me@rommelrico.com>
2018-10-10 10:24:06 -0700

    Mutable Map in Kotlin.

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Maps.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Maps.kt
@@ -10,7 +10,15 @@ fun main(args: Array<String>) {
     )
     println(immutableMap.javaClass) // class java.util.LinkedHashMap
     println(immutableMap)
-    
+
+    // Mutable Map in Kotlin.
+    val mutableMap = mutableMapOf(
+            "John's Car" to Car("red", "Range Rover", 2010),
+            "Jane's Car" to Car("blue", "Hyundai", 2012)
+    )
+    println(mutableMap.javaClass) // class java.util.LinkedHashMap
+    println(mutableMap)
+
 }
 
 data class Car(val color: String, val model: String, val year: Int)
