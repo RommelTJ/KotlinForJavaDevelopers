2fb822940b970d804d062c868a49cbba65906765
Rommel Rico <me@rommelrico.com>
2018-10-09 21:05:23 -0700

    Mutable vs Immutable lists

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Collections.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Collections.kt
@@ -11,4 +11,13 @@ fun main(args: Array<String>) {
     // List of not null
     val notNullList = listOfNotNull("hello", null, "goodbye")
     println(notNullList) // prints hello and goodbye
+
+    // This is a mutable list, but has no methods to modify it.
+    val arrayList = arrayListOf(1, 2, 3)
+    println(arrayList.javaClass) // prints class java.util.ArrayList
+
+    // This is a mutable list.
+    val mutableList = mutableListOf(1, 2, 3)
+    println(mutableList.javaClass) // prints class java.util.ArrayList
+
 }
