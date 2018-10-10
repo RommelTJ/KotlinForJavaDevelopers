82f71cad0492b8effb6272c55b54e720e5d77274
Rommel Rico <me@rommelrico.com>
2018-10-10 08:22:58 -0700

    Getting a maximum value in a collection.

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Lists.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Lists.kt
@@ -4,6 +4,7 @@ fun main(args: Array<String>) {
 
     val strings = listOf("spring", "summer", "fall", "winter")
     val colorList = listOf("black", "white", "green")
+    val ints = listOf(1, 2, 4, 6, 9, 3)
 
     // Getting last element in a List.
     println(strings.last())
@@ -12,5 +13,7 @@ fun main(args: Array<String>) {
     // Getting an item if it exists.
     println(strings.getOrNull(5))
     println(strings.getOrNull(2))
+    // Getting a maximum value in a collection.
+    println(ints.max())
 
 }
