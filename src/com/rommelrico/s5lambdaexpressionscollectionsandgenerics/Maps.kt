d10726b7a8156f0932ba796302d753be34e9b1c4
Rommel Rico <me@rommelrico.com>
2018-10-10 10:19:28 -0700

    Car class

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Maps.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Maps.kt
@@ -2,6 +2,6 @@ package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
 
 fun main(args: Array<String>) {
 
-    
-
 }
+
+data class Car(val color: String, val model: String, val year: Int)
