72e7aec5d11ba1ab0d3fa90fe077c2a02446b0a6
Rommel Rico <me@rommelrico.com>
2018-10-09 21:00:34 -0700

    Kotlin Empty List

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Collections.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Collections.kt
@@ -3,6 +3,8 @@ package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
 fun main(args: Array<String>) {
     // Immutable List<String>. Immutable in Kotlin, but not in Java.
     val strings = listOf("spring", "summer", "fall", "winter")
-    println(strings.javaClass)
-    
+    println(strings.javaClass) // class java.util.Arrays$ArrayList
+
+    val emptyList = emptyList<String>()
+    println(emptyList.javaClass) // class kotlin.collections.EmptyList
 }
