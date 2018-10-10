d30f9f8b531503f47e5572071452c5c9bcb06bba
Rommel Rico <me@rommelrico.com>
2018-10-09 20:59:23 -0700

    Additional Note on immutability in Kotlin vs Java

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Collections.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Collections.kt
@@ -1,7 +1,8 @@
 package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
 
 fun main(args: Array<String>) {
-    // Immutable List<String>.
+    // Immutable List<String>. Immutable in Kotlin, but not in Java.
     val strings = listOf("spring", "summer", "fall", "winter")
     println(strings.javaClass)
+    
 }
