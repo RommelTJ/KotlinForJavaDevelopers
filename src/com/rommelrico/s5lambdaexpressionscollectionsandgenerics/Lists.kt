80fa3b0d954a4f43f94fc5a7218b67d0a02cd53e
Rommel Rico <me@rommelrico.com>
2018-10-10 08:18:52 -0700

    Lists file

---
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Lists.kt
@@ -0,0 +1,8 @@
+package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
+
+fun main(args: Array<String>) {
+
+    val strings = listOf("spring", "summer", "fall", "winter")
+    val colorList = listOf("black", "white", "green")
+
+}
