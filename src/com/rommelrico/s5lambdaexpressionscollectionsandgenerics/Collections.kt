c373c32f0be00cc70020f717d70af96faf6eaa35
Rommel Rico <me@rommelrico.com>
2018-10-09 20:58:19 -0700

    Kotlin Immutable List of Strings

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Collections.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Collections.kt
@@ -1,5 +1,7 @@
 package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
 
 fun main(args: Array<String>) {
-    
+    // Immutable List<String>.
+    val strings = listOf("spring", "summer", "fall", "winter")
+    println(strings.javaClass)
 }
