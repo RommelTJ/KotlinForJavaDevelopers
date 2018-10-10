0dbebdde1850073747dc9abd88b7489ea60de190
Rommel Rico <me@rommelrico.com>
2018-10-10 10:21:08 -0700

    Immutable Map in Kotlin

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Maps.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Maps.kt
@@ -2,6 +2,13 @@ package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
 
 fun main(args: Array<String>) {
 
+    // Immutable Map in Kotlin.
+    val immutableMap = mapOf(
+            1 to Car("green", "Toyota", 2015),
+            2 to Car("red", "Ford", 2016),
+            3 to Car("silver", "Honda", 2013)
+    )
+
 }
 
 data class Car(val color: String, val model: String, val year: Int)
