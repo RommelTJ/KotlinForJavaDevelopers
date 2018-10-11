c01a97516e87bc77fd77dadc63ee865e453f38fb
Rommel Rico <me@rommelrico.com>
2018-10-11 08:34:28 -0700

    Getting odd numbers from set using a filter

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/CollectionsStuff.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/CollectionsStuff.kt
@@ -2,11 +2,12 @@ package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
 
 fun main(args: Array<String>) {
     val setInts = setOf(10, 15, 19, 5, 3, -22)
+    println(setInts.filter { it % 2 != 0 }) // only odd numbers
 
     val immutableMap = mapOf(
             1 to Car("green", "Toyota", 2015),
             2 to Car("red", "Ford", 2016),
             3 to Car("silver", "Honda", 2013)
     )
-    
+
 }
