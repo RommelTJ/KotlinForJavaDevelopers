1dc66353afa1266150cab3335ab0044dc5ff8455
Rommel Rico <me@rommelrico.com>
2018-10-11 08:32:32 -0700

    CollectionsStuff class

---
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/CollectionsStuff.kt
@@ -0,0 +1,12 @@
+package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
+
+fun main(args: Array<String>) {
+    val setInts = setOf(10, 15, 19, 5, 3, -22)
+
+    val immutableMap = mapOf(
+            1 to Car("green", "Toyota", 2015),
+            2 to Car("red", "Ford", 2016),
+            3 to Car("silver", "Honda", 2013)
+    )
+    
+}
