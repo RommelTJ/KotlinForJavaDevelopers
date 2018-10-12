3cd743964e398fafc66ef0db2d71ab9b7f83d07b
Rommel Rico <me@rommelrico.com>
2018-10-12 08:43:19 -0700

    Working with Generics using Kotlin Extension functions

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Generics.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Generics.kt
@@ -8,16 +8,25 @@ fun main(args: Array<String>) {
     list[0].toUpperCase()
     list.add("Another string")
     printCollection(list)
+    list.printCollection2()
 
     // Using any kind of List.
     val bdList = mutableListOf(BigDecimal(-33.45), BigDecimal(3503.99), BigDecimal(0.329))
     printCollection(bdList)
+    bdList.printCollection2()
 
 }
 
 // Accepting any type of List.
 fun <T> printCollection(collection: List<T>) {
     for (item in collection) {
+        println(item)
+    }
+}
+
+// We could've also fixed this with an extension function.
+fun <T> List<T>.printCollection2() {
+    for (item in this) {
         println(item)
     }
 }
