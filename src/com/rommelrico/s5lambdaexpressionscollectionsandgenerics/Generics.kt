d827f5783ccfa78649ab4da3888e189f58628197
Rommel Rico <me@rommelrico.com>
2018-10-12 08:38:27 -0700

    How to use Generics in Kotlin functions

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Generics.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Generics.kt
@@ -4,5 +4,13 @@ fun main(args: Array<String>) {
 
     val list: MutableList<String> = mutableListOf("Hello")
     list[0].toUpperCase()
+    list.add("Another string")
+    printCollection(list)
 
+}
+
+fun printCollection(collection: List<String>) {
+    for (item in collection) {
+        println(item)
+    }
 }
