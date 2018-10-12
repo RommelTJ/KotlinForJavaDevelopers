25f614409f15b23b81af7449886198f7e552ecda
Rommel Rico <me@rommelrico.com>
2018-10-12 08:39:59 -0700

    Working with Kotlin types

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Generics.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Generics.kt
@@ -1,11 +1,17 @@
 package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
 
+import java.math.BigDecimal
+
 fun main(args: Array<String>) {
 
     val list: MutableList<String> = mutableListOf("Hello")
     list[0].toUpperCase()
     list.add("Another string")
     printCollection(list)
+
+    // Not able to print a non generic list.
+    val bdList = mutableListOf(BigDecimal(-33.45), BigDecimal(3503.99), BigDecimal(0.329))
+    printCollection(bdList)
 
 }
 
