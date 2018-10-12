eb77735b4b39b22b495a1a8f849e07978238570f
Rommel Rico <me@rommelrico.com>
2018-10-12 08:34:13 -0700

    Java Generics

---
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/javacode/JavaGenerics.java
@@ -0,0 +1,13 @@
+package com.rommelrico.s5lambdaexpressionscollectionsandgenerics.javacode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaGenerics {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Hello");
+    }
+
+}
