d77d87ce945f643d85b56f00792dae198e4b8c71
Rommel Rico <me@rommelrico.com>
2018-10-12 08:14:28 -0700

    Setting up the Sequences file

---
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Sequences.kt
@@ -0,0 +1,11 @@
+package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
+
+fun main(args: Array<String>) {
+    val immutableMap = mapOf(
+            1 to Car("green", "Toyota", 2015),
+            2 to Car("red", "Ford", 2016),
+            3 to Car("silver", "Honda", 2013),
+            17 to Car("red", "BMW", 2015),
+            8 to Car("green", "Ford", 2010)
+    )
+}
