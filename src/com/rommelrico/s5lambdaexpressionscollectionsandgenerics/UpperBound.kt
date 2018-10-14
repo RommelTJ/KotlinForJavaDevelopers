56cc00ae69a6dc7cebb84be6ff3f2e49395f2964
Rommel Rico <me@rommelrico.com>
2018-10-13 21:57:45 -0700

    Working with nullable and non-nullable options

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/UpperBound.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/UpperBound.kt
@@ -6,23 +6,33 @@ import java.lang.StringBuilder
 fun main(args: Array<String>) {
 
     val ints = listOf(1, 2, 3, 4, 5)
-    val shorts: List<Short> = listOf(10, 20, 30, 40)
-    val floats: List<Float> = listOf(100.3f, -459.43f)
+    val shorts: List<Short?> = listOf(10, 20, 30, 40)
+    val floats: List<Float?> = listOf(100.3f, -459.43f)
+    val strings = listOf("1", "2", "3")
 
     convertToInt(ints)
     convertToInt(shorts)
     convertToInt(floats)
 
     append(StringBuilder("String 1"), StringBuilder("String 2"))
-    
+
+    printCollection(shorts)
+    printCollection(strings)
+
 }
 
-fun <T: Number> convertToInt(collection: List<T>) {
+fun <T: Number?> convertToInt(collection: List<T>) {
     for (num in collection) {
-        println("${num.toInt()}")
+        println("${num?.toInt()}")
     }
 }
 
 fun <T> append(item1: T, item2: T) where T: CharSequence, T: Appendable {
     println("Result is ${item1.append(item2)}")
+}
+
+fun <T: Any> printCollection(collection: List<T>) {
+    for (item in collection) {
+        println(item)
+    }
 }
