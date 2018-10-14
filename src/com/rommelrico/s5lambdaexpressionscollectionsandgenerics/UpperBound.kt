b8f3f7a5eef0c258107666ad075e4ccf1c5e53c3
Rommel Rico <me@rommelrico.com>
2018-10-13 21:45:41 -0700

    UpperBound class

---
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/UpperBound.kt
@@ -0,0 +1,8 @@
+package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
+
+fun main(args: Array<String>) {
+
+    val ints = listOf(1, 2, 3, 4, 5)
+    val shorts: List<Short> = listOf(10, 20, 30, 40)
+
+}
