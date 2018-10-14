801ce05407e9d3fb653c9f3f5b1eb3dcb69d9533
Rommel Rico <me@rommelrico.com>
2018-10-13 22:01:14 -0700

    Generics are a compile-time feature

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/javacode/JavaGenerics.java
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/javacode/JavaGenerics.java
@@ -8,6 +8,9 @@ public class JavaGenerics {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
         list.add("Hello");
+
+        boolean b = list instanceof List;
+        
     }
 
 }
