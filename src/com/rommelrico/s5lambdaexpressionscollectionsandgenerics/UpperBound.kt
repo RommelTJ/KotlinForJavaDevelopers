795d3cbf83fe77d039a8da2e27386bcac084608f
Rommel Rico <me@rommelrico.com>
2018-10-13 21:50:10 -0700

    Testing convertToInt with Floats

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/UpperBound.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/UpperBound.kt
@@ -4,9 +4,11 @@ fun main(args: Array<String>) {
 
     val ints = listOf(1, 2, 3, 4, 5)
     val shorts: List<Short> = listOf(10, 20, 30, 40)
+    val floats: List<Float> = listOf(100.3f, -459.43f)
 
     convertToInt(ints)
     convertToInt(shorts)
+    convertToInt(floats)
 
 }
 
