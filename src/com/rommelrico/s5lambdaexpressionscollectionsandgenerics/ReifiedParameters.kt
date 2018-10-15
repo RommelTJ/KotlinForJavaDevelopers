a923dbdfb3d5d62f63bd7bcb8317c42960e7891b
Rommel Rico <me@rommelrico.com>
2018-10-14 20:16:32 -0700

    getElementsOfType function

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/ReifiedParameters.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/ReifiedParameters.kt
@@ -1,5 +1,24 @@
 package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
 
+import java.math.BigDecimal
+
 fun main(args: Array<String>) {
-    
+
+    val mixedList: List<Any> = listOf("string", 1, BigDecimal(22.5), "fall", BigDecimal(-5938.398))
+    val bigDecimalsOnly = getElementsOfType<BigDecimal>(mixedList)
+    println(bigDecimalsOnly)
+
+}
+
+// Reified function.
+inline fun <reified T> getElementsOfType(list: List<Any>): List<T> {
+
+    var newList: MutableList<T> = mutableListOf()
+    for (element in list) {
+        if (element is T) {
+            newList.add(element)
+        }
+    }
+
+    return newList
 }
