8abb1a0847425b3c3efab5841f360789986bc30a
Rommel Rico <me@rommelrico.com>
2018-10-15 08:12:30 -0700

    Setting up a rose garden instance

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Contravariance.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Contravariance.kt
@@ -2,6 +2,11 @@ package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
 
 fun main(args: Array<String>) {
 
+    val roseTender = object: FlowerCare<Rose> {
+        override fun prune(flower: Rose) = println("I'm pruning a rose!")
+    }
+    val roseGarden2 = Garden2<Rose>(listOf(Rose(), Rose()), roseTender)
+
 }
 
 class Garden2<T: Flower>(val flowers: List<T>, val flowerCare: FlowerCare<T>) {
