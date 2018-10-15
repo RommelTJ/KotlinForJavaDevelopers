bbbb8837b2b1aaade3925720c4c1715f3c008293
Rommel Rico <me@rommelrico.com>
2018-10-15 08:24:41 -0700

    Adding a name property to the Flower2 class

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Contravariance.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Contravariance.kt
@@ -3,14 +3,14 @@ package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
 // Contravariance is opposite of Covariance. It means accepting all subclasses, vs preserving the type.
 fun main(args: Array<String>) {
 
-    val flowerTender = object: FlowerCare<Flower> {
-        override fun prune(flower: Flower) = println("I'm tending a flower!")
+    val flowerTender = object: FlowerCare<Flower2> {
+        override fun prune(flower: Flower2) = println("I'm tending a ${flower.name}!")
     }
 
     val roseTender = object: FlowerCare<Rose> {
         override fun prune(flower: Rose) = println("I'm pruning a rose!")
     }
-    val roseGarden2 = Garden2<Rose>(listOf(Rose(), Rose()), roseTender)
+    val roseGarden2 = Garden2<Rose2>(listOf(Rose2(), Rose2()), roseTender)
     roseGarden2.tendFlower(0)
 
     val daffodilTender = object: FlowerCare<Daffodil> {
@@ -21,7 +21,9 @@ fun main(args: Array<String>) {
 
 }
 
-class Garden2<T: Flower>(val flowers: List<T>, val flowerCare: FlowerCare<T>) {
+open class Flower2(val name: String)
+
+class Garden2<T: Flower2>(val flowers: List<T>, val flowerCare: FlowerCare<T>) {
 
     fun pickFlower(i: Int) = flowers[i]
 
@@ -31,7 +33,9 @@ class Garden2<T: Flower>(val flowers: List<T>, val flowerCare: FlowerCare<T>) {
 
 }
 
-class Daffodil: Flower()
+class Rose2: Flower2("Rose")
+
+class Daffodil: Flower2("Daffodil")
 
 // Making FlowerCare Contravariant by adding the in keyword.
 interface FlowerCare<in T> {
