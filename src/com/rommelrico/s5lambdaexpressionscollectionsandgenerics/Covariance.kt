5037382493ec5f3314713d63f8b5e22f678a7ae1
Rommel Rico <me@rommelrico.com>
2018-10-14 20:21:20 -0700

    Convert To Int function

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Covariance.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Covariance.kt
@@ -1,5 +1,12 @@
 package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
 
 fun main(args: Array<String>) {
-    
+    val shortList: List<Short> = listOf(1, 2, 3, 4, 5)
+    println(convertToInt(shortList))
+}
+
+fun convertToInt(collection: List<Number>) {
+    for (num in collection) {
+        println("${num.toInt()}")
+    }
 }
