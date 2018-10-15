c66afbd6ec0846f5a032f25de6d28cb8bda11143
Rommel Rico <me@rommelrico.com>
2018-10-14 20:22:01 -0700

    Working with mutable lists

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Covariance.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Covariance.kt
@@ -1,11 +1,11 @@
 package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
 
 fun main(args: Array<String>) {
-    val shortList: List<Short> = listOf(1, 2, 3, 4, 5)
+    val shortList: MutableList<Short> = mutableListOf(1, 2, 3, 4, 5)
     println(convertToInt(shortList))
 }
 
-fun convertToInt(collection: List<Number>) {
+fun convertToInt(collection: MutableList<Number>) {
     for (num in collection) {
         println("${num.toInt()}")
     }
