90f1b1fdae31b62dcb73b37c927ffe8acea5a11a
Rommel Rico <me@rommelrico.com>
2018-10-15 08:33:10 -0700

    Adding some sample code

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/UseSiteVariance.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/UseSiteVariance.kt
@@ -3,3 +3,15 @@ package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
 fun main(args: Array<String>) {
 
 }
+
+fun copyCars(source: MutableList<Car2>, destination: MutableList<Car2>) {
+    for (car in source) {
+        destination.add(car)
+    }
+}
+
+open class Car2
+
+class Toyota: Car2()
+
+class Ford: Car2()
