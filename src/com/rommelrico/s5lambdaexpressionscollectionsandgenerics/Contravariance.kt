f1cc2fe7e1f5f5589cf9787b88a9b217ab8ed006
Rommel Rico <me@rommelrico.com>
2018-10-15 08:11:01 -0700

    Garden2 class

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Contravariance.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Contravariance.kt
@@ -4,6 +4,10 @@ fun main(args: Array<String>) {
 
 }
 
+class Garden2<T: Flower>(val flowers: List<T>, val flowerCare: FlowerCare<T>) {
+    fun pickFlower(i: Int) = flowers[i]
+}
+
 interface FlowerCare<T> {
     fun prune(flower: T)
 }
