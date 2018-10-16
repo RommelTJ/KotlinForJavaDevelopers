26a46ea584741554c9a8762cfb9986071bc23619
Rommel Rico <me@rommelrico.com>
2018-10-16 08:23:52 -0700

    Adding some more people

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Challenge5.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Challenge5.kt
@@ -5,6 +5,11 @@ data class Person(val firstName: String, val lastName: String, val age: Int)
 
 fun main(args: Array<String>) {
     val joe = Person("Joe", "Jones", 45)
+    val jane = Person("Jane", "Smith", 12)
+    val mary = Person("Mary", "Wilson", 70)
+    val john = Person("John", "Adams", 32)
+    val jean = Person("Jean", "Smithson", 66)
+    
     val (fName, lName, age) = joe
     println("fName = $fName, lName = $lName, age = $age")
 }
