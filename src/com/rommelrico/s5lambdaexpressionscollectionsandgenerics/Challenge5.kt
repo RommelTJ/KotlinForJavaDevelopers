5e92b7881c7383a39df9f4f9f13ca9bb91fec2f1
Rommel Rico <me@rommelrico.com>
2018-10-16 08:22:01 -0700

    Solution to challenge 1

--- src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Challenge5.kt
+++ src/com/rommelrico/s5lambdaexpressionscollectionsandgenerics/Challenge5.kt
@@ -1,9 +1,10 @@
 package com.rommelrico.s5lambdaexpressionscollectionsandgenerics
 
-class Person(val firstName: String, val lastName: String, val age: Int)
+// Challenge 1
+data class Person(val firstName: String, val lastName: String, val age: Int)
 
 fun main(args: Array<String>) {
     val joe = Person("Joe", "Jones", 45)
-//    val (fName, lName, age) = joe
-//    println("fName = $fName, lName = $lName, age = $age")
+    val (fName, lName, age) = joe
+    println("fName = $fName, lName = $lName, age = $age")
 }
