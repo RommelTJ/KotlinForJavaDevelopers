51a4e3cbd284421599561e9a27a36cdef04d7bcd
Rommel Rico <me@rommelrico.com>
2018-10-17 08:46:27 -0700

    Another way of reading from a file

--- src/com/rommelrico/s6fileio/FileReading.kt
+++ src/com/rommelrico/s6fileio/FileReading.kt
@@ -7,4 +7,9 @@ fun main(args: Array<String>) {
     val lines = File("testfile.txt").reader().readLines()
     lines.forEach { println(it) }
 
+    val reader = File("testfile.txt").reader()
+    val lines2 = reader.readText() // This needs to be closed.
+    println(lines2)
+    reader.close() // closing the resource.
+
 }
