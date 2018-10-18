214a6b60f43072f9ccdd398d7519bedb0cfbe3dc
Rommel Rico <me@rommelrico.com>
2018-10-18 10:01:21 -0700

    Java Dummy Class

---
+++ src/com/rommelrico/s6fileio/javacode/DummyClass.java
@@ -0,0 +1,9 @@
+package com.rommelrico.s6fileio.javacode;
+
+public class DummyClass {
+
+    public static void main(String[] args) {
+        System.out.println("I'm just here so there's a Java file");
+    }
+
+}
