73b342a84cd4802aeb4dd487f6fc7e64261afa98
Rommel Rico <me@rommelrico.com>
2018-10-18 09:55:44 -0700

    Reading from a File using Kotlin

--- src/com/rommelrico/s6fileio/BinaryIO.kt
+++ src/com/rommelrico/s6fileio/BinaryIO.kt
@@ -1,5 +1,22 @@
 package com.rommelrico.s6fileio
 
+import java.io.DataInputStream
+import java.io.EOFException
+import java.io.FileInputStream
+
+// Reading from Binary Files is just using the Java IO behind the scenes.
 fun main(args: Array<String>) {
+
+    val di = DataInputStream(FileInputStream("testfile.bin"))
+    var si: String
+
+    try {
+        while (true) {
+            si = di.readUTF()
+            println(si)
+        }
+    } catch (e: EOFException) {
+        println("EOFException: " + e.message)
+    }
 
 }
