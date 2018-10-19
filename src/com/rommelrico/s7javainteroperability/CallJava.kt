a7a3d1486c899e55f6b9c5b8d294e30b1975f87e
Rommel Rico <me@rommelrico.com>
2018-10-19 08:15:51 -0700

    Creating a Kotlin car variable using the Java Car class

--- src/com/rommelrico/s7javainteroperability/CallJava.kt
+++ src/com/rommelrico/s7javainteroperability/CallJava.kt
@@ -1,5 +1,9 @@
 package com.rommelrico.s7javainteroperability
 
+import com.rommelrico.s7javainteroperability.javacode.Car
+
 fun main(args: Array<String>) {
+
+    val car = Car("blue", "Ford", 2015)
 
 }
