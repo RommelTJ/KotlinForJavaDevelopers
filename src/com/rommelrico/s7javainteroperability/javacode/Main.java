001de7e52c65a3ffe3c9a22ebb83982390865ff6
Rommel Rico <me@rommelrico.com>
2018-10-20 21:03:55 -0700

    Calling a top level Kotlin function from the Java Class

--- src/com/rommelrico/s7javainteroperability/javacode/Main.java
+++ src/com/rommelrico/s7javainteroperability/javacode/Main.java
@@ -1,9 +1,13 @@
 package com.rommelrico.s7javainteroperability.javacode;
 
+// Notice the 'Kt' at the end of the import.
+import com.rommelrico.s7javainteroperability.KotlinCarKt;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        // Calling a top level Kotlin function from the Java Class.
+        KotlinCarKt.topLevel();
     }
 
 }
