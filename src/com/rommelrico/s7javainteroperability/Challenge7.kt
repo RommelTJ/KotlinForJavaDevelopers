1f961ef0dcc049d6f6ef028050288022a60d425f
Rommel Rico <me@rommelrico.com>
2018-10-21 18:42:38 -0700

    Part 1 solution

--- src/com/rommelrico/s7javainteroperability/Challenge7.kt
+++ src/com/rommelrico/s7javainteroperability/Challenge7.kt
@@ -1,5 +1,12 @@
 package com.rommelrico.s7javainteroperability
 
+import com.rommelrico.s7javainteroperability.javacode.Employee
+
 fun main(args: Array<String>) {
-    
+
+    // Part 1.
+    val employee = Employee("Jane", "Smith", 2000)
+    employee.lastName = "Jones"
+    employee.salaryLast3Years = floatArrayOf(50000.25f, 54000.60f, 56000.48f)
+
 }
